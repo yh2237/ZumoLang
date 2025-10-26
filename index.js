745a1ea16f179bb6229d2abfa8ed6c539849374f
yh2237 <2237.yh.2237@gmail.com>
2025-10-27 04:59:46 +0900

    .gitignoreとpackage.jsonの更新、config.ymlの追加、インタプリタ機能の改善

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const fs = require("fs");
-const path = require("path");
+const yaml = require('js-yaml');
 const tokenize = require("./src/tokenizer.js");
 const Interpreter = require("./src/interpreter.js");
 
-const code = fs.readFileSync(process.argv[2], 'utf-8');
+const config = yaml.load(fs.readFileSync('config/config.yml', 'utf8'));
 
-try {
-    const tokens = tokenize(code);
+if (!process.argv[2]) {
+    console.log("How to use: zumo <File Path> <debug>");
+    process.exit(0);
+} else if (process.argv[2] === "-v" || process.argv[2] === "--version") {
+    console.log(`Version: ${config.version}`);
+    process.exit(0);
+} else {
 
-    if (process.argv[3]) {
-        console.log("[DEBUG]Tokens:", tokens);
+    const code = fs.readFileSync(process.argv[2], 'utf-8');
+
+    try {
+        const tokens = tokenize(code);
+
+        if (process.argv[3]) {
+            console.log("[DEBUG]Tokens:", tokens);
+        }
+        const interp = new Interpreter();
+        interp.execute(tokens);
+    } catch (e) {
+        console.error("[FATAL ERROR]", e.message);
     }
-    const interp = new Interpreter();
-    interp.execute(tokens);
-} catch (e) {
-    console.error("[FATAL ERROR]", e.message);
 }
